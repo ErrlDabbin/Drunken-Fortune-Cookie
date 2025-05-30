02686ba68276b411382bd288076a933f50a3c6ce
ErrlDabbin <121827637+ErrlDabbin@users.noreply.github.com>
2025-05-29 17:44:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // Helper function to generate image URL dynamically based on the deployment
